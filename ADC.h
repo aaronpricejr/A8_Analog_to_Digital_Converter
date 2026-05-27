c977308cf55adf64a993cb4cbc3b583d25cf6159
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-05-27 16:33:59 -0700

    Add header file for ADC module

---
+++ ADC.h
@@ -0,0 +1,24 @@
+/**
+ ******************************************************************************
+ * @file       	: ADC.h
+ * @brief      	: Header for ADC module from A8 - ADC
+ * 					  (Analog to Digital Converter)
+ * project     	: EE 329 S'26 A5
+ * authors     	: Aaron Price Jr. & Brandon Valenti
+ * version     	: 0.1
+ * date        	: 2026-05-19
+ * compiler    	: STM32CubeIDE v.1.19.0
+ * target      	: NUCLEO-L4A6ZG
+ * clocks      	: 4 MHz MSI to AHB2
+ ******************************************************************************
+ */
+
+#ifndef INC_ADC_H_
+#define INC_ADC_H_
+
+#include "stm32l4xx_hal.h"
+
+void ADC_Init(void);
+
+
+#endif /* INC_ADC_H_ */
