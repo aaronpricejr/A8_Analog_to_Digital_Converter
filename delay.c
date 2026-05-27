312f03c4cc4ff67ff83d2b93788b0b8cc99e1ba5
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-05-27 16:55:05 -0700

    Add delay functions using SysTick timer
    Implement SysTick timer initialization and delay functions.

---
+++ delay.c
@@ -0,0 +1,40 @@
+/*
+ *******************************************************************************
+ * @file           : delay.c
+ * @brief          :
+ * project         : EE 329 S'26 A3
+ * authors         : Aaron Price Jr
+ * version         : 0.1
+ * date            : 260415
+ * compiler        : STM32CubeIDE v.1.19.0 Build: 14980_20230301_1550 (UTC)
+ * target          : NUCLEO-L4A6ZG
+ * clocks          : 4 MHz MSI to AHB2
+ * @attention      : (c) 2026 STMicroelectronics.  All rights reserved.
+ *******************************************************************************
+ * Header format adapted from [Code Appendix by Kevin Vo] pg 5
+ */
+#include "delay.h"
+
+void SysTick_Init(void) {
+	SysTick->CTRL |= (SysTick_CTRL_ENABLE_Msk |     	// enable SysTick Timer
+                      SysTick_CTRL_CLKSOURCE_Msk); 	// select CPU clock
+	SysTick->CTRL &= ~(SysTick_CTRL_TICKINT_Msk);  	// disable interrupt
+}
+
+// delay in microseconds using SysTick timer to count CPU clock cycles
+// do not call with 0 : error, maximum delay.
+// careful calling with small nums : results in longer delays than specified:
+//	   e.g. @4MHz, delay_us(1) = 10-15 us delay.
+void delay_us(const uint32_t time_us) {
+	// set the counts for the specified delay
+	SysTick->LOAD = (uint32_t)((time_us * (SystemCoreClock / 1000000)) - 1);
+	SysTick->VAL = 0;                                  	 // clear timer count
+	SysTick->CTRL &= ~(SysTick_CTRL_COUNTFLAG_Msk);    	 // clear count flag
+	while (!(SysTick->CTRL & SysTick_CTRL_COUNTFLAG_Msk)); // wait for flag
+}
+
+void delay_ms(const uint32_t time_ms) {
+   for(uint32_t i = 0; i < time_ms; i++) {
+      delay_us(1000);
+   }
+}
