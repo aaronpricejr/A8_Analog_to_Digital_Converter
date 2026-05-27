16d9fc4b458adf602f1f029b7aa7499fb4bcb114
BrandonValenti <brandonvalenti111@gmail.com>
2026-05-27 16:44:20 -0700

    Create lpuart.h

---
+++ lpuart.h
@@ -0,0 +1,13 @@
+/**
+******************************************************************************
+* @file : lpuart.h
+* @brief : Header for lpuart.c file.
+*
+******************************************************************************
+*/
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef LPUART_H
+#define LPUART_H
+void LPUART_init(void);
+void LPUART_print(const char *message);
+#endif
