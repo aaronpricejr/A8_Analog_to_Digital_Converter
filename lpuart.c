0b2abeee666199bdc4f11048e3498a398af8708b
BrandonValenti <brandonvalenti111@gmail.com>
2026-05-28 12:12:12 -0700

    Update lpuart.c

--- lpuart.c
+++ lpuart.c
@@ -1,136 +1,180 @@
 /**
-******************************************************************************
-* @file : lpuart.c
-* @brief : Main program body
-*
-******************************************************************************
-*/
-/* Includes ------------------------------------------------------------------*/
+ ******************************************************************************
+ * @file    : lpuart.c
+ * @brief   : LPUART1 initialization and ADC table display
+ ******************************************************************************
+ */
+
 #include "stm32l4xx.h"
 #include "lpuart.h"
 
-void LPUART_init(void) {
-// Enable VDDIO2 for GPIOG pins PG[15:2]
-PWR->CR2 |= PWR_CR2_IOSV;
-// Enable GPIOG clock
-RCC->AHB2ENR |= RCC_AHB2ENR_GPIOGEN;
-// Enable LPUART1 clock
-RCC->APB1ENR2 |= RCC_APB1ENR2_LPUART1EN;
-// PG7 = LPUART1_TX, PG8 = LPUART1_RX, AF8
-// Set PG7 and PG8 to alternate function mode: MODER = 10
-GPIOG->MODER &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
-GPIOG->MODER |= ((2U << (7 * 2)) | (2U << (8 * 2)));
-// Output type: push-pull
-GPIOG->OTYPER &= ~((1U << 7) | (1U << 8));
-// High speed
-GPIOG->OSPEEDR &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
-GPIOG->OSPEEDR |= ((3U << (7 * 2)) | (3U << (8 * 2)));
-// Pull-up resistors
-GPIOG->PUPDR &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
-GPIOG->PUPDR |= ((1U << (7 * 2)) | (1U << (8 * 2)));
-// Select AF8 for PG7 and PG8
-// PG7 is in AFR[0]
-GPIOG->AFR[0] &= ~(0xFU << (7 * 4));
-GPIOG->AFR[0] |= (8U << (7 * 4));
-// PG8 is in AFR[1], position 0
-GPIOG->AFR[1] &= ~(0xFU << ((8 - 8) * 4));
-GPIOG->AFR[1] |= (8U << ((8 - 8) * 4));
-// Disable LPUART before configuration
-LPUART1->CR1 &= ~USART_CR1_UE;
-// 8-bit data
-LPUART1->CR1 &= ~(USART_CR1_M1 | USART_CR1_M0);
+void LPUART_init(void)
+{
+    /* Enable VDDIO2 for GPIOG pins PG[15:2] */
+    PWR->CR2 |= PWR_CR2_IOSV;
 
-// 1 stop bit
-LPUART1->CR2 &= ~USART_CR2_STOP;
-// No parity
-LPUART1->CR1 &= ~USART_CR1_PCE;
-// Baud rate: 115200 with 4 MHz clock
-LPUART1->BRR = 0xD056;
-// Enable transmitter and receiver
-LPUART1->CR1 |= USART_CR1_TE | USART_CR1_RE;
-// Enable LPUART
-LPUART1->CR1 |= USART_CR1_UE;
-}
+    /* Enable GPIOG clock */
+    RCC->AHB2ENR |= RCC_AHB2ENR_GPIOGEN;
 
-void LPUART_print(const char *message) {
-uint16_t i = 0;
-while (message[i] != '\0') {
-while (!(LPUART1->ISR & USART_ISR_TXE))
-;
-LPUART1->TDR = message[i];
-i++;
-}
+    /* Enable LPUART1 clock */
+    RCC->APB1ENR2 |= RCC_APB1ENR2_LPUART1EN;
+
+    /*
+     * PG7 = LPUART1_TX
+     * PG8 = LPUART1_RX
+     * AF8 = LPUART1
+     */
+
+    GPIOG->MODER &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
+    GPIOG->MODER |=  ((2U << (7 * 2)) | (2U << (8 * 2)));
+
+    GPIOG->OTYPER &= ~((1U << 7) | (1U << 8));
+
+    GPIOG->OSPEEDR &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
+    GPIOG->OSPEEDR |=  ((3U << (7 * 2)) | (3U << (8 * 2)));
+
+    GPIOG->PUPDR &= ~((3U << (7 * 2)) | (3U << (8 * 2)));
+    GPIOG->PUPDR |=  ((1U << (7 * 2)) | (1U << (8 * 2)));
+
+    GPIOG->AFR[0] &= ~(0xFU << (7 * 4));
+    GPIOG->AFR[0] |=  (8U << (7 * 4));
+
+    GPIOG->AFR[1] &= ~(0xFU << ((8 - 8) * 4));
+    GPIOG->AFR[1] |=  (8U << ((8 - 8) * 4));
+
+    LPUART1->CR1 &= ~USART_CR1_UE;
+
+    LPUART1->CR1 &= ~(USART_CR1_M1 | USART_CR1_M0);
+    LPUART1->CR2 &= ~USART_CR2_STOP;
+    LPUART1->CR1 &= ~USART_CR1_PCE;
+
+    /* 115200 baud with 4 MHz clock */
+    LPUART1->BRR = 0xD056;
+
+    LPUART1->CR1 |= USART_CR1_TE | USART_CR1_RE;
+    LPUART1->CR1 |= USART_CR1_UE;
 }
 
 static void LPUART_print_char(char c)
 {
-    while (!(LPUART1->ISR & USART_ISR_TXE)) { ; }
+    while (!(LPUART1->ISR & USART_ISR_TXE)) { }
     LPUART1->TDR = c;
 }
 
-static void LPUART_print_uint_width(uint32_t value, uint8_t width)
+void LPUART_print(const char *message)
 {
-    char buf[10];
-    uint8_t len = 0;
+    uint16_t i = 0;
 
-    if (value == 0)
+    while (message[i] != '\0')
     {
-        buf[len++] = '0';
+        LPUART_print_char(message[i]);
+        i++;
     }
-    else
-    {
-        uint32_t v = value;
-        while (v > 0)
-        {
-            buf[len++] = '0' + (v % 10);
-            v /= 10;
-        }
-    }
-
-    for (uint8_t i = len; i < width; i++)
-        LPUART_print_char(' ');
-
-    for (int8_t i = len - 1; i >= 0; i--)
-        LPUART_print_char(buf[i]);
 }
 
-static void LPUART_print_voltage(uint16_t count)
+char LPUART_read_char(void)
 {
-    uint32_t mv    = ((uint32_t)count * 3300UL) / 4095UL;
-    uint32_t whole = mv / 1000;
-    uint32_t frac  = mv % 1000;
+    char c;
 
-    LPUART_print_char(' ');
-    LPUART_print_char('0' + (char)whole);
+    while (!(LPUART1->ISR & USART_ISR_RXNE)) { }
+
+    c = (char)(LPUART1->RDR & 0xFF);
+
+    /*
+     * Treat newline as Enter too.
+     * Some terminals send '\n', others send '\r'.
+     */
+    if (c == '\n')
+    {
+        c = '\r';
+    }
+
+    return c;
+}
+
+void LPUART_clear_screen(void)
+{
+    LPUART_print("\033[2J");   /* clear entire terminal */
+    LPUART_print("\033[H");    /* move cursor to top-left */
+}
+
+static void LPUART_cursor_home(void)
+{
+    LPUART_print("\033[H");
+}
+
+static void LPUART_print_4digit_counts(uint16_t value)
+{
+    uint16_t divisor;
+    uint8_t digit;
+
+    if (value > 9999)
+    {
+        value = 9999;
+    }
+
+    divisor = 1000;
+
+    while (divisor > 0)
+    {
+        digit = value / divisor;
+        LPUART_print_char((char)('0' + digit));
+        value = value % divisor;
+        divisor = divisor / 10;
+    }
+}
+
+static void LPUART_print_voltage_mv(int32_t mv)
+{
+    uint32_t whole;
+    uint32_t frac;
+
+    if (mv < 0)
+    {
+        mv = 0;
+    }
+
+    if (mv > 3000)
+    {
+        mv = 3000;
+    }
+
+    whole = (uint32_t)mv / 1000;
+    frac  = (uint32_t)mv % 1000;
+
+    LPUART_print_char((char)('0' + whole));
     LPUART_print_char('.');
-    LPUART_print_char('0' + (char)(frac / 100));
-    LPUART_print_char('0' + (char)((frac / 10) % 10));
-    LPUART_print_char('0' + (char)(frac % 10));
-    LPUART_print_char(' ');
-    LPUART_print_char('V');
+    LPUART_print_char((char)('0' + (frac / 100)));
+    LPUART_print_char((char)('0' + ((frac / 10) % 10)));
+    LPUART_print_char((char)('0' + (frac % 10)));
+    LPUART_print(" V");
 }
 
-static void LPUART_print_row(const char *label, uint16_t count)
+static void LPUART_print_ADC_row(const char *label,
+                                 uint16_t counts,
+                                 int32_t mv)
 {
     LPUART_print(label);
-    LPUART_print_char(' ');
-    LPUART_print_char(' ');
-    LPUART_print_uint_width((uint32_t)count, 4);
-    LPUART_print_char(' ');
-    LPUART_print_char(' ');
-    LPUART_print_voltage(count);
-    LPUART_print_char('\r');
-    LPUART_print_char('\n');
+    LPUART_print("  ");
+    LPUART_print_4digit_counts(counts);
+    LPUART_print("  ");
+    LPUART_print_voltage_mv(mv);
+    LPUART_print("     ");
+    LPUART_print("\r\n");
 }
 
-void LPUART_print_ADC_table(uint16_t adc_min, uint16_t adc_max, uint32_t adc_avg)
+void LPUART_print_ADC_table(uint16_t min_counts,
+                            uint16_t max_counts,
+                            uint16_t avg_counts,
+                            int32_t min_mv,
+                            int32_t max_mv,
+                            int32_t avg_mv)
 {
-    LPUART_print("\033[H");   // move cursor to top-left, do NOT clear screen
+    LPUART_cursor_home();
 
-    LPUART_print("ADC  counts  volts\r\n");
+    LPUART_print("ADC counts volts\r\n");
 
-    LPUART_print_row("MIN", adc_min);
-    LPUART_print_row("MAX", adc_max);
-    LPUART_print_row("AVG", (uint16_t)adc_avg);
+    LPUART_print_ADC_row("MIN", min_counts, min_mv);
+    LPUART_print_ADC_row("MAX", max_counts, max_mv);
+    LPUART_print_ADC_row("AVG", avg_counts, avg_mv);
 }
-
