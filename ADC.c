d0c315b7e1ec30ecd45f3bba6bb85b26bb22e8f6
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-05-27 17:47:42 -0700

    Update ADC.c

--- ADC.c
+++ ADC.c
@@ -1,50 +1,62 @@
 /**
  ******************************************************************************
- * @file       	: ADC.c
- * @brief      	: A8 - ADC (Analog to Digital Converter)
- * project     	: EE 329 S'26 A5
- * authors     	: Aaron Price Jr. & Brandon Valenti
- * version     	: 0.1
- * date        	: 2026-05-19
- * compiler    	: STM32CubeIDE v.1.19.0
- * target      	: NUCLEO-L4A6ZG
- * clocks      	: 4 MHz MSI to AHB2
+ * @file    : ADC.c
+ * @brief   : ADC initialization and configuration
+ * project  : EE 329 S'26 A5
+ * authors  : Aaron Price Jr. & Brandon Valenti
+ * version  : 0.1
+ * date     : 2026-05-19
+ * compiler : STM32CubeIDE v.1.19.0
+ * target   : NUCLEO-L4A6ZG
  ******************************************************************************
  */
 
 #include "ADC.h"
 
-void ADC_Init() {
-	RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;         // turn on clock for ADC
-	// power up & calibrate ADC
-	ADC123_COMMON->CCR |= (1 << ADC_CCR_CKMODE_Pos); // clock source = HCLK/1
-	ADC1->CR &= ~(ADC_CR_DEEPPWD);             // disable deep-power-down
-	ADC1->CR |= (ADC_CR_ADVREGEN);             // enable V regulator - see RM 18.4.6
-	delay_us(20);                              // wait 20us for ADC to power up
-	ADC1->DIFSEL &= ~(ADC_DIFSEL_DIFSEL_5);    // PA0=ADC1_IN5, single-ended
-	ADC1->CR &= ~(ADC_CR_ADEN | ADC_CR_ADCALDIF); // disable ADC, single-end calib
-	ADC1->CR |= ADC_CR_ADCAL;                  // start calibration
-	while (ADC1->CR & ADC_CR_ADCAL) {;}        // wait for calib to finish
-	// enable ADC
-	ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
-	ADC1->CR |= ADC_CR_ADEN;                   // enable ADC
-	while(!(ADC1->ISR & ADC_ISR_ADRDY)) {;}    // wait for ADC Ready flag
-	ADC1->ISR |= (ADC_ISR_ADRDY);              // set to clr ADC Ready flag
-	// configure ADC sampling & sequencing
-	ADC1->SQR1  |= (5 << ADC_SQR1_SQ1_Pos);    // sequence = 1 conv., ch 5
-	ADC1->SMPR1 |= (1 << ADC_SMPR1_SMP5_Pos);  // ch 5 sample time = 6.5 clocks
-	ADC1->CFGR  &= ~( ADC_CFGR_CONT  |         // single conversion mode
-	                  ADC_CFGR_EXTEN |         // h/w trig disabled for s/w trig
-	                  ADC_CFGR_RES   );        // 12-bit resolution
-	// configure & enable ADC interrupt
-	ADC1->IER |= ADC_IER_EOCIE;                // enable end-of-conv interrupt
-	ADC1->ISR |= ADC_ISR_EOC;                  // set to clear EOC flag
-	NVIC->ISER[0] = (1<<(ADC1_2_IRQn & 0x1F)); // enable ADC interrupt service
-	__enable_irq();                            // enable global interrupts
-	// configure GPIO pin PA0
-	RCC->AHB2ENR  |= (RCC_AHB2ENR_GPIOAEN);    // connect clock to GPIOA
-	GPIOA->MODER  |= (GPIO_MODER_MODE0);       // analog mode for PA0 (set MODER last)
+volatile uint16_t adc_result   = 0;
+volatile uint8_t  adc_eoc_flag = 0;
 
-	ADC1->CR |= ADC_CR_ADSTART;                // start 1st conversion
+void delay_us(uint32_t us)
+{
+    for (volatile uint32_t i = 0; i < (us * 4); i++) { ; }
+}
 
+void ADC_init(void)
+{
+    /* Clock & power */
+    RCC->AHB2ENR |= RCC_AHB2ENR_ADCEN;
+    ADC123_COMMON->CCR |= (1 << ADC_CCR_CKMODE_Pos);
+    ADC1->CR &= ~ADC_CR_DEEPPWD;
+    ADC1->CR |=  ADC_CR_ADVREGEN;
+    delay_us(20);
+
+    /* Calibration */
+    ADC1->DIFSEL &= ~ADC_DIFSEL_DIFSEL_5;
+    ADC1->CR &= ~(ADC_CR_ADEN | ADC_CR_ADCALDIF);
+    ADC1->CR |=  ADC_CR_ADCAL;
+    while (ADC1->CR & ADC_CR_ADCAL) { ; }
+
+    /* Enable ADC */
+    ADC1->ISR |= ADC_ISR_ADRDY;
+    ADC1->CR  |= ADC_CR_ADEN;
+    while (!(ADC1->ISR & ADC_ISR_ADRDY)) { ; }
+    ADC1->ISR |= ADC_ISR_ADRDY;
+
+    /* Sampling & sequencing */
+    ADC1->SQR1  |= (5 << ADC_SQR1_SQ1_Pos);
+    ADC1->SMPR1 |= (1 << ADC_SMPR1_SMP5_Pos);
+    ADC1->CFGR  &= ~(ADC_CFGR_CONT | ADC_CFGR_EXTEN | ADC_CFGR_RES);
+
+    /* EOC interrupt */
+    ADC1->IER |= ADC_IER_EOCIE;
+    ADC1->ISR |= ADC_ISR_EOC;
+    NVIC->ISER[0] = (1 << (ADC1_2_IRQn & 0x1F));
+    __enable_irq();
+
+    /* GPIO PA0 analog input */
+    RCC->AHB2ENR  |= RCC_AHB2ENR_GPIOAEN;
+    GPIOA->MODER  |= GPIO_MODER_MODE0;
+
+    /* Start first conversion */
+    ADC1->CR |= ADC_CR_ADSTART;
 }
