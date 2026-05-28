a9f720ea42ae8c88a0a0c2f79271864fc3df9324
Aaron Price Jr <156409292+aaronpricejr@users.noreply.github.com>
2026-05-27 17:25:03 -0700

    Add prototype for LPUART_print_ADC_table function
    Added function prototype for LPUART_print_ADC_table.

--- lpuart.h
+++ lpuart.h
@@ -1,13 +1,11 @@
-/**
-******************************************************************************
-* @file : lpuart.h
-* @brief : Header for lpuart.c file.
-*
-******************************************************************************
-*/
-/* Define to prevent recursive inclusion -------------------------------------*/
 #ifndef LPUART_H
 #define LPUART_H
+
+#include <stdint.h>
+
+// FUNCTION PROTOTYPES ---------------------------------------------------------
 void LPUART_init(void);
 void LPUART_print(const char *message);
-#endif
+void LPUART_print_ADC_table(uint16_t adc_min, uint16_t adc_max, uint32_t adc_avg);
+
+#endif /* LPUART_H */
